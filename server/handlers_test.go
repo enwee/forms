4e3f5d032b584e8e2f9dc88774128320e9950b65
enwee <tanenwee@gmail.com>
2020-12-09 16:50:58 +0800

    Refactor test code

--- server/handlers_test.go
+++ server/handlers_test.go
@@ -25,46 +25,118 @@ func init() {
 	}
 }
 
-func TestMakeFormModes(t *testing.T) {
-	formItems := []formItem{
-		{"Order", "select", []string{"Chicken", "Fish", "Pork", "Beef"}},
-		{"Qty", "select", []string{"1", "2", "3"}},
-		{"Chilli packs", "checkbox", nil},
-		{"Disposable cutlery", "checkbox", nil},
-		{"Comments", "text", nil},
-		{"", "text", nil},
-		{"Name", "text", nil},
-		{"Email", "text", nil},
-		{"Contact", "text", nil},
-		{"Delivery address (if any)", "text", nil},
-		{"Self collection", "checkbox", nil},
-	}
+var baseWithEmptyLabel = []formItem{
+	{"Order", "select", []string{"Chicken", "Fish", "Pork", "Beef"}},
+	{"Qty", "select", []string{"1", "2", "3"}},
+	{"Chilli packs", "checkbox", nil},
+	{"Disposable cutlery", "checkbox", nil},
+	{"Comments", "text", nil},
+	{"", "text", nil},
+	{"Name", "text", nil},
+	{"Email", "text", nil},
+	{"Contact", "text", nil},
+	{"Delivery address (if any)", "text", nil},
+	{"Self collection", "checkbox", nil},
+}
 
-	tests := []struct {
-		name, action string
-		editMode     bool
-		scrap        func(io.Reader) form
-	}{
-		{"Preview Mode", "view", false, scrapPreviewBody},
-		{"Edit Mode", "edit", true, scrapEditBody},
+type test struct {
+	name, action, title string
+	formItems, expected []formItem
+	editMode            bool
+	scrape              func(io.Reader) form
+}
+
+func TestMakeFormPreview(t *testing.T) {
+	tests := []test{
+		{
+			name:      "Empty Label",
+			title:     "Lam's BBQ Order",
+			formItems: baseWithEmptyLabel,
+		},
+		{
+			name:      "Empty Title",
+			title:     "",
+			formItems: baseWithEmptyLabel,
+		},
+		{
+			name:  "Empty Option",
+			title: "Lam's",
+			formItems: []formItem{
+				{"Order", "select", []string{""}},
+				{"Contact", "text", nil},
+			},
+		},
+		{
+			name:  "Empty Label with Select Options",
+			title: "Lam's",
+			formItems: []formItem{
+				{"", "select", []string{"1", "2", "3"}},
+				{"Contact", "text", nil},
+			},
+		},
 	}
 
 	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			data := form{"Lam's BBQ Order Form", formItems, test.editMode}
-			w := httptest.NewRecorder()
-			r, err := http.NewRequest("POST", "/", makeBody(data, test.action))
-			if err != nil {
-				t.Fatal(err)
-			}
-			r.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-
-			app.makeForm(w, r)
-			resp := w.Result()
-			scrapped := test.scrap(resp.Body)
-			if !reflect.DeepEqual(data, scrapped) {
-				t.Errorf("\nrequest(expected):\n%+v\nscrapped(received):\n%+v", data, scrapped)
-			}
-		})
+		test.action = "view"
+		test.editMode = false
+		test.scrape = scrapePreviewPage
+		runTest(test, t)
 	}
+}
+
+func TestMakeFormEdit(t *testing.T) {
+	tests := []test{
+		{
+			name:      "Empty Label",
+			title:     "Lam's BBQ Order",
+			formItems: baseWithEmptyLabel,
+		},
+		{
+			name:      "Empty Title",
+			title:     "",
+			formItems: baseWithEmptyLabel,
+		},
+		{
+			name:  "Empty Option",
+			title: "Lam's",
+			formItems: []formItem{
+				{"Order", "select", []string{""}},
+				{"Contact", "text", nil},
+			},
+		},
+		{
+			name:  "Empty Label with Select Options",
+			title: "Lam's",
+			formItems: []formItem{
+				{"", "select", []string{"1", "2", "3"}},
+				{"Contact", "text", nil},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		test.action = "edit"
+		test.editMode = true
+		test.scrape = scrapeEditPage
+		runTest(test, t)
+	}
+}
+
+func runTest(test test, t *testing.T) {
+	t.Run(test.name, func(t *testing.T) {
+		data := form{test.title, test.formItems, test.editMode}
+		w := httptest.NewRecorder()
+		r, err := http.NewRequest("POST", "/", makeBody(data, test.action))
+		if err != nil {
+			t.Fatal(err)
+		}
+		r.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+		app.makeForm(w, r)
+		resp := w.Result()
+		scrapped := test.scrape(resp.Body)
+		if !reflect.DeepEqual(data, scrapped) {
+			t.Errorf("\nrequest(expected):\n%+v\nscrapped(received):\n%+v", data, scrapped)
+		}
+	})
 }
--- server/testutils_test.go
+++ server/testutils_test.go
@@ -8,6 +8,7 @@ import (
 	"golang.org/x/net/html"
 )
 
+// makeBody strings together a form request body e.g key=value&key=value&....
 func makeBody(data form, action string) io.Reader {
 	body := "action=" + action + "&title=" + data.Title
 	for index, formItem := range data.FormItems {
@@ -24,11 +25,13 @@ func makeBody(data form, action string) io.Reader {
 	return strings.NewReader(body)
 }
 
+// getNextToken advances to next token and returns the Token
 func getNextToken(z *html.Tokenizer) html.Token {
 	z.Next()
 	return z.Token()
 }
 
+// getAttr returns the value of given attibuite in the Token
 func getAttr(t html.Token, key string) string {
 	for _, attr := range t.Attr {
 		if attr.Key == key {
@@ -38,6 +41,7 @@ func getAttr(t html.Token, key string) string {
 	return "[" + key + "] not found"
 }
 
+// checkEditMode returns the editMode based on state of Edit/Preview buttons
 func checkEditMode(t html.Token) bool {
 	m := map[string]string{}
 	for _, attr := range t.Attr {
@@ -58,42 +62,93 @@ func checkEditMode(t html.Token) bool {
 	return false
 }
 
-func scrapPreviewBody(body io.Reader) form {
-	scrapped := form{}
+// scrapePreviewPage screen scrapes the response Body and returns a form struct for test comparison
+func scrapePreviewPage(body io.Reader) form {
+	scraped := form{}
+	processedOptions := false
+	t := html.Token{}
 	z := html.NewTokenizer(body)
+	// this extracts the only the Edit/Preview button state and form Title
+	// dont need the loop to to keep checking for this after initial
 	for {
-		t := getNextToken(z)
+		t = getNextToken(z)
 		if z.Err() != nil {
 			break
 		}
 		if t.Type == html.StartTagToken && t.Data == "button" {
-			scrapped.EditMode = checkEditMode(t)
+			scraped.EditMode = checkEditMode(t)
 			continue
 		}
 		if t.Type == html.StartTagToken && t.Data == "h1" {
-			t = getNextToken(z) // go into text inside <h1>
-			scrapped.Title = t.Data
+			// go into text inside <h1> or if <h1></h1>
+			t = getNextToken(z)
+			if t.Type == html.EndTagToken && t.Data == "h1" {
+				scraped.Title = ""
+				break
+			}
+			scraped.Title = t.Data
 			break
 		}
 	}
-
+	// this extracts the rest of the form items
+	// label, input type, and options if <select>
 	for {
-		t := getNextToken(z)
-		if z.Err() != nil {
-			break
+		// empty <label></label> of select input type has hidden options
+		// <input type=hidden name=optionsX> are scrapped until <label> tag
+		// if previous iteration processedOptions, dont advance to next Token
+		// and in this case process last/current Token
+		if !processedOptions {
+			t = getNextToken(z)
+			if z.Err() != nil {
+				break
+			}
 		}
+		processedOptions = false // reset flag for current iteration
+
 		if t.Type == html.StartTagToken && t.Data == "label" {
 			fI := formItem{}
-
-			t = getNextToken(z) // go into text inside <label> or if empty
-			if t.Type == html.EndTagToken && t.Data == "label" {
-				fI.Type = "text"
-				scrapped.FormItems = append(scrapped.FormItems, fI)
+			// go into text inside <label> or if <label></label>
+			t = getNextToken(z)
+			if t.Type == html.EndTagToken && t.Data == "label" { // if empty
+				for {
+					t = getNextToken(z)
+					if z.Err() != nil {
+						break
+					}
+					// next token must be <input type="hidden" name="type">
+					inputTag := t.Type == html.StartTagToken && t.Data == "input"
+					attrs := getAttr(t, "type") == "hidden" && getAttr(t, "name") == "type"
+					if inputTag && attrs {
+						fI.Type = getAttr(t, "value")
+						break
+					}
+				}
+				if fI.Type == "select" {
+					for {
+						t = getNextToken(z)
+						if z.Err() != nil {
+							break
+						}
+						// next token must be <input type="hidden" name="optionsX">
+						inputTag := t.Type == html.StartTagToken && t.Data == "input"
+						hidden := getAttr(t, "type") == "hidden"
+						optionX := getAttr(t, "name") == "options"+strconv.Itoa(len(scraped.FormItems))
+						if inputTag && hidden && optionX {
+							fI.Options = append(fI.Options, getAttr(t, "value"))
+						} else if t.Type == html.StartTagToken && t.Data == "label" {
+							// next iteration dont advance to next Token
+							// process current Token on next iteration
+							processedOptions = true
+							break
+						}
+					}
+				}
+				scraped.FormItems = append(scraped.FormItems, fI)
 				continue
 			}
 			fI.Label = t.Data
-
-			for { // scan to next <select> or <input>
+			// look for next <select> or <input> for input type
+			for {
 				t = getNextToken(z)
 				if z.Err() != nil {
 					break
@@ -102,7 +157,7 @@ func scrapPreviewBody(body io.Reader) form {
 					break
 				}
 			}
-
+			// <select> is followed by <option>s
 			if t.Data == "select" {
 				fI.Type = t.Data
 				for {
@@ -111,36 +166,41 @@ func scrapPreviewBody(body io.Reader) form {
 						break
 					}
 					if t.Type == html.StartTagToken && t.Data == "option" {
-						t = getNextToken(z) // go into text inside <option>
+						t = getNextToken(z) // go into text inside <option> or if empty
+						if t.Type == html.EndTagToken && t.Data == "option" {
+							fI.Options = append(fI.Options, "") // empty case
+							continue
+						}
 						fI.Options = append(fI.Options, t.Data)
 					} else if t.Type == html.EndTagToken && t.Data == "select" {
 						break
 					}
 				}
 			}
-
+			// text or checkbox type
 			if t.Data == "input" {
 				fI.Type = getAttr(t, "type")
 			}
-
-			scrapped.FormItems = append(scrapped.FormItems, fI)
+			scraped.FormItems = append(scraped.FormItems, fI)
 		}
 	}
-	return scrapped
+	return scraped
 }
 
-func scrapEditBody(body io.Reader) form {
-	scrapped := form{}
-	didOptions := false
+func scrapeEditPage(body io.Reader) form {
+	scraped := form{}
+	processedOptions := false
 	t := html.Token{}
 	z := html.NewTokenizer(body)
+	// this extracts the only the Edit/Preview button state and form Title
+	// dont need the loop to to keep checking for this after initial
 	for {
 		t = getNextToken(z)
 		if z.Err() != nil {
 			break
 		}
 		if t.Type == html.StartTagToken && t.Data == "button" {
-			scrapped.EditMode = checkEditMode(t)
+			scraped.EditMode = checkEditMode(t)
 			continue
 		}
 		if t.Type == html.StartTagToken && t.Data == "h1" {
@@ -150,7 +210,7 @@ func scrapEditBody(body io.Reader) form {
 					break
 				}
 				if t.Type == html.StartTagToken && t.Data == "input" {
-					scrapped.Title = getAttr(t, "value")
+					scraped.Title = getAttr(t, "value")
 				} else if t.Type == html.EndTagToken && t.Data == "h1" {
 					break
 				}
@@ -158,25 +218,29 @@ func scrapEditBody(body io.Reader) form {
 			break
 		}
 	}
-
+	// this extracts the rest of the form items
+	// label, input type, and options if <select>
 	for {
-		if !didOptions {
+		// options are scrapped until next <input name!=optionsX>
+		// if previous iteration processedOptions, dont advance to next Token
+		// and in this case process last/current Token
+		if !processedOptions {
 			t = getNextToken(z)
 			if z.Err() != nil {
 				break
 			}
 		}
-		didOptions = false // cant think of a way to reverse z.Next
+		processedOptions = false // reset flag for current iteration
 
 		if t.Type == html.StartTagToken && t.Data == "input" {
 			fI := formItem{}
-
+			// look for next <input name="label">
 			if getAttr(t, "name") != "label" {
 				continue
 			}
 			fI.Label = getAttr(t, "value")
-
-			for { // scan to next <input>
+			// look for next <input name="type">
+			for {
 				t = getNextToken(z)
 				if z.Err() != nil {
 					break
@@ -188,26 +252,28 @@ func scrapEditBody(body io.Reader) form {
 					}
 				}
 			}
-
+			// <input name="type" value="select">
 			if fI.Type == "select" {
 				fI.Options = []string{}
-				for { // scan to next <input>
+				for { // look for next <input name=optionsX>
 					t = getNextToken(z)
 					if z.Err() != nil {
 						break
 					}
 					if t.Type == html.StartTagToken && t.Data == "input" {
-						if getAttr(t, "name") == "options"+strconv.Itoa(len(scrapped.FormItems)) {
+						if getAttr(t, "name") == "options"+strconv.Itoa(len(scraped.FormItems)) {
 							fI.Options = append(fI.Options, getAttr(t, "value"))
 						} else {
-							didOptions = true // have when to next wanted token
+							// next iteration dont advance to next Token
+							// process current Token on next iteration
+							processedOptions = true
 							break
 						}
 					}
 				}
 			}
-			scrapped.FormItems = append(scrapped.FormItems, fI)
+			scraped.FormItems = append(scraped.FormItems, fI)
 		}
 	}
-	return scrapped
+	return scraped
 }
